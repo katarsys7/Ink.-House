526cbfba610d5ab76a8724c50fb47640fb3f3c1a
katarsys7 <94797649+katarsys7@users.noreply.github.com>
2023-12-04 13:28:34 +0500

    Update shop.js

--- shop.js
+++ shop.js
@@ -9,19 +9,3 @@ SwitchMode.onclick = function() {
         theme.href = "shop.css"
     }
 }
-const exist_element=document.querySelector("input");
-
-exist_element.addEventListener("click",()=>{
-    let new_element=document.createElement("div");
-    new_element.innerHTML="А теперь сюда --->";
-    new_element.classList.add("divxd");
-    exist_element.before(new_element);
-    let new_element_close=document.createElement("input");
-    new_element_close.type="button";
-    new_element_close.className="green_button";
-    new_element_close.value="Х";
-    new_element_close.addEventListener("click",()=>{
-        new_element.remove();
-    })
-    new_element.append(new_element_close);
-})
